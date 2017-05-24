444a2877a6b198d2817413a35bd80c87d4e53d04
kaori <kaori55194@gmail.com>
2017-05-23 18:26:29 -0700

    Add correspondence for Activity Life Cycle
    correspondence for Activity Life Cycle.
    when change Visual devise direction Horizontal or
    vertical.

--- PracticeCountryQuiz/app/src/main/java/com/example/kaorihirata/practicecountryquiz/MainActivity.java
+++ PracticeCountryQuiz/app/src/main/java/com/example/kaorihirata/practicecountryquiz/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kaorihirata.practicecountryquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -14,26 +15,36 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton mNextButton;
     private ImageButton mPreviousButton;
     private TextView mQuestionTextView;
-//    private Question[] mQuestionBank = new Question[] { //下記と同様のもの（書き方が違うだけ）
+    //    private Question[] mQuestionBank = new Question[] { //下記と同様のもの（書き方が違うだけ）
     private Question[] mQuestionBank = {
-            new Question(R.string.question_canada,false),
-            new Question(R.string.question_japan,false),
-            new Question(R.string.question_us,false),
-            new Question(R.string.question_korea,false),
+            new Question(R.string.question_canada, false),
+            new Question(R.string.question_japan, false),
+            new Question(R.string.question_us, false),
+            new Question(R.string.question_korea, false),
 //        new Question  (TextView) findViewById(R.id.question_france);
     };
 
-    private int mCurrentIndex=0; //　何番目の質問か取得する為のもの
+    private int mCurrentIndex = 0; //　何番目の質問か取得する為のもの
+    private static final String TAG = "QuizActivity";
+    private static final String KEY = "index"; // for Activity Life Cycle
 
     //メインのコード
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        //Activity Life Cycle 対応
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY, 0);
+        }
+
+        //質問表示
+        mQuestionTextView = (TextView) findViewById(R.id.question_text);
+        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId()); //デフォルト質問または、前回の質問を表示。
 
 
-        mQuestionTextView =(TextView) findViewById(R.id.question_text);
-        mQuestionTextView.setText(mQuestionBank[0].getTextResId()); //デフォルト質問を表示。
         mQuestionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,9 +82,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // previous button pressed!
                 mCurrentIndex = mCurrentIndex - 1;
-                if(mCurrentIndex<0)
-                {
-                    mCurrentIndex = mQuestionBank.length-1; // lenghtは要素数
+                if (mCurrentIndex < 0) {
+                    mCurrentIndex = mQuestionBank.length - 1; // lenghtは要素数
                 }
                 updateQuestion();
             }
@@ -89,25 +99,62 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+
+    }
+
+    //Activity Life Cycle 対応（appを閉じる前のindex番号を取得しておく）
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY, mCurrentIndex);
     }
 
     //メインのサポート
-    private void updateQuestion(){
+    private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
     }
 
-    private void checkAnswer(boolean userPressedTrue){
+    private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTure();
         int messageResId = 0;
-        if(answerIsTrue==userPressedTrue){
+        if (answerIsTrue == userPressedTrue) {
             messageResId = R.string.correct_toast;
-        }
-        else {
+        } else {
             messageResId = R.string.incorrect_toast;
         }
-        Toast toast= Toast.makeText(MainActivity.this, messageResId, Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(MainActivity.this, messageResId, Toast.LENGTH_SHORT);
         toast.show();
         toast.setGravity(0, 0, 0);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
