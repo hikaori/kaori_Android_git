82201409214fd92103bcc0e837c8460b2d97e895
kaori <kaori55194@gmail.com>
2017-05-26 02:25:36 -0700

    Add function
    First, back to default code. then add below functions.
    
    ・Add a Listener to the TextView so that a user could press the TextView
    itself to see the next question.
    ・Add a Previous Button/ Next Button.

---
+++ MyQuestionApprication/MyCountryQuiz/app/src/main/java/com/derrick/park/countryquiz/Questions.java
@@ -0,0 +1,19 @@
+package com.derrick.park.countryquiz;
+
+/**
+ * Created by kaorihirata on 2017-05-25.
+ */
+
+public class Questions {
+    private int mQuestionStringID;
+    private boolean mQuestionAnswer;
+
+    public Questions(int QId,boolean QA) {
+        this.mQuestionStringID = QId;
+        this.mQuestionAnswer = QA;
+    }
+
+    public int getQuestionStringID() {
+        return mQuestionStringID;
+    }
+}
--- MyQuestionApprication/MyCountryQuiz/app/src/main/java/com/derrick/park/countryquiz/QuizActivity.java
+++ MyQuestionApprication/MyCountryQuiz/app/src/main/java/com/derrick/park/countryquiz/QuizActivity.java
@@ -4,17 +4,45 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+    private TextView mQuestionArea;
     private Button mTureButton;
     private Button mFalseButton;
+    private Button mPreviousButton;
+    private Button mNextButton;
+    private int mCurrentQuestionIndex = 0;
+
+    Questions Q1 = new Questions(R.string.question_text1, true);
+    Questions Q2 = new Questions(R.string.question_text2, true);
+    Questions Q3 = new Questions(R.string.question_text3, true);
+    Questions Q4 = new Questions(R.string.question_text4, true);
+    Questions Q5 = new Questions(R.string.question_text5, true);
+    private Questions[] mQuestionBank = {Q1, Q2, Q3, Q4, Q5};
+//    String[] wordArray = {"Apple", "Orange", "Pear", "Kiwi", "Banana"}; // Array instantiation reference
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mQuestionArea = (TextView) findViewById(R.id.question_area);
+        mQuestionArea.setText(mQuestionBank[0].getQuestionStringID());
+        mQuestionArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //** increment "mCurrentQuestionIndex", handle happening when "mCurrentQuestionIndex" count over Question's Number using "if"
+//                mCurrentQuestionIndex++;
+//                if (mCurrentQuestionIndex == mQuestionBank.length) {
+//                    mCurrentQuestionIndex = 0;
+//                }
+                mCurrentQuestionIndex = (mCurrentQuestionIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
 
         mTureButton = (Button) findViewById(R.id.true_button);
         mTureButton.setOnClickListener(new View.OnClickListener() {
@@ -29,17 +57,43 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast t = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_LONG);
-//                Toast t = new Toast(QuizActivity.this);
-//                t=Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_LONG);
-                t.show();
-                t.setGravity(0,0,0);
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_LONG).show();
             }
         });
 
+        mPreviousButton = (Button) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentQuestionIndex--;
+                if (mCurrentQuestionIndex < 0) {
+                    mCurrentQuestionIndex = mQuestionBank.length - 1;
+                }
 
+                mCurrentQuestionIndex = (mCurrentQuestionIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
 
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //** increment "mCurrentQuestionIndex", handle happening when "mCurrentQuestionIndex" count over Question's Number using "if"
+//                mCurrentQuestionIndex++;
+//                if (mCurrentQuestionIndex == mQuestionBank.length) {
+//                    mCurrentQuestionIndex = 0;
+//                }
+                mCurrentQuestionIndex = (mCurrentQuestionIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+    }
 
-
+    private void updateQuestion() {
+        int QuestionNum = mQuestionBank[mCurrentQuestionIndex].getQuestionStringID();
+        mQuestionArea.setText(QuestionNum);
     }
 }
+
+
